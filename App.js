2b9db10647f0b37cc8e5372f94a588b054423716
K Mak <kumarajiva08@gmail.com>
2018-07-29 18:10:24 +1000

    deck list

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DeckList from './components/DeckList';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!!</Text>
+        <DeckList />
       </View>
     );
   }
---
+++ components/DeckList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View, StyleSheet }  from 'react-native';
+import { getData } from '../utils/api';
+
+export default class DeckList extends React.Component {
+  render() {
+    const decks  = getData();
+    return (
+      <View>
+        {Object.keys(decks).map(deckKey => {
+          const { title, questions } = decks[deckKey];
+          return (
+            <View key={deckKey}>
+              <Text>{title}</Text>
+              <Text>{questions.length}</Text>
+            </View>
+          )
+        })}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
